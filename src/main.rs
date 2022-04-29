8bef4b6e79c2d741886818c821772289bbad6e9d
Eric Hagman <eric@instawork.com>
2022-04-29 13:25:22 -0400

    Week 2 Updates

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
+use std::fmt;
 use std::io::{self, stdout, Write};
 
 struct Board {
     word: String,
-    rows: [[char; 5]; 6],
+    rows: [[Cell; 5]; 6],
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Cell {
+    Green(char),
+    Yellow(char),
+    Gray(char),
+    Empty,
+}
+
+impl Cell {
+    pub fn new() -> Self {
+        Cell::Empty
+    }
 }
 
 impl Board {
     pub fn new() -> Self {
         Board {
             word: "rusty".to_string(),
-            rows: [['-'; 5]; 6],
+            rows: [[Cell::new(); 5]; 6],
         }
+    }
+    pub fn guess_cell(&self, guess_idx: usize, c: char) -> Cell {
+        let mut existing = false;
+        for (word_idx, letter) in self.word.chars().enumerate() {
+            if guess_idx == word_idx && c == letter {
+                return Cell::Green(c);
+            } else if guess_idx != word_idx && c == letter {
+                return Cell::Yellow(c);
+            }
+            if c == letter {
+                existing = true;
+            }
+        }
+        if !existing {
+            return Cell::Gray(c);
+        }
+        return Cell::Empty;
     }
     // self, &self, mut self, &mut self
     pub fn guess(&mut self, guess: &str) {
@@ -23,7 +55,7 @@ impl Board {
         let index = self
             .rows
             .iter()
-            .position(|&r| r[0] == '-')
+            .position(|&r| r[0] == Cell::Empty)
             .expect("You lose!");
         // ['r', 'o', 'u', 't', 'e']
         // ['-', '-', '-', '-', '-']
@@ -35,11 +67,25 @@ impl Board {
         if guess.len() > 5 {
             panic!("can't guess more than 5")
         }
+        // atlye
         let chars = guess.chars();
-        for (idx, c) in chars.enumerate() {
-            println!("{} {} {}", idx, c, c as u32);
-            self.rows[index][idx] = c;
+        for (guess_idx, c) in chars.enumerate() {
+            println!("{} {} {}", guess_idx, c, c as u32);
+            let cell = self.guess_cell(guess_idx, c);
+            self.rows[index][guess_idx] = cell;
         }
+    }
+}
+
+impl fmt::Debug for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for r in &self.rows {
+            for c in r {
+                write!(f, "{:?} | ", c);
+            }
+            write!(f, "\n");
+        }
+        Ok(())
     }
 }
 
@@ -54,6 +100,6 @@ fn main() {
             .expect("failed to read guess");
 
         board.guess(&input);
-        println!("{:?}", board.rows);
+        println!("{:?}", board);
     }
 }
